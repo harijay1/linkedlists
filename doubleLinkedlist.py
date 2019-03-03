35a0193e58bb0070a28da511ede2f8d1230d9bf1
Hariharan  Jayaraman <manakncorp@iMac.local>
2019-03-02 19:49:02 -0500

    Welcome Message Initial commit

--- doubleLinkedlist.py
+++ doubleLinkedlist.py
@@ -1,1 +1,11 @@
 class ListNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+        self.node_id = None
+
+
+def main():
+    print("Welcome to Doubly Linked Lists")
+main()
